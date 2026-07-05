4cfd272a32d7919529ed67640819492253f351c5
jeg <126290814+jaikaar15@users.noreply.github.com>
2026-07-05 14:26:43 -0400

    Create fictionalelements.js

---
+++ fictionalelements.js
@@ -0,0 +1,23 @@
+elements.vibranium = {
+  color: "#444b57",
+  behavior: behaviors.WALL,
+  category: "solids",
+  state: "solid",
+  tempHigh: 3400,
+  stateHigh: "molten_vibranium"
+};
+
+elements.molten_vibranium = {
+  color: "#c73b08",
+  behavior: behaviors.MOLTEN,
+  category: "states",
+  state: "liquid",
+  temp: 3400,
+  tempHigh: 10000,
+  stateHigh: "fire",
+  tempLow: 3200,
+  stateLow: "vibranium",
+  conduct: 0.95,
+  density: 4000,
+  viscosity: 8000
+};
