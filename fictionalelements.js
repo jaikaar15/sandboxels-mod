05d251a386934025cda0af1a7e63bbd93a050eb1
jeg <126290814+jaikaar15@users.noreply.github.com>
2026-07-05 14:35:41 -0400

    Update fictionalelements.js

--- fictionalelements.js
+++ fictionalelements.js
@@ -1,23 +1,28 @@
-elements.vibranium = {
-  color: "#444b57",
-  behavior: behaviors.WALL,
-  category: "solids",
-  state: "solid",
-  tempHigh: 3400,
-  stateHigh: "molten_vibranium"
-};
+// Wait for the game engine to be ready
+window.addEventListener('runAfterAutoload', function() {
+    
+    elements.vibranium = {
+        color: "#444b57",
+        behavior: behaviors.WALL,
+        category: "solids",
+        state: "solid",
+        tempHigh: 3400,
+        stateHigh: "molten_vibranium"
+    };
 
-elements.molten_vibranium = {
-  color: "#c73b08",
-  behavior: behaviors.MOLTEN,
-  category: "states",
-  state: "liquid",
-  temp: 3400,
-  tempHigh: 10000,
-  stateHigh: "fire",
-  tempLow: 3200,
-  stateLow: "vibranium",
-  conduct: 0.95,
-  density: 4000,
-  viscosity: 8000
-};
+    elements.molten_vibranium = {
+        color: "#c73b08",
+        behavior: behaviors.MOLTEN,
+        category: "states",
+        state: "liquid",
+        temp: 3400,
+        tempHigh: 10000,
+        stateHigh: "fire",
+        tempLow: 3200,
+        stateLow: "vibranium",
+        conduct: 0.95,
+        density: 4000,
+        viscosity: 8000
+    };
+
+});
